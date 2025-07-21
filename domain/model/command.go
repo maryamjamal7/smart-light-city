f1a59ea9e64ad25c09878db2596356fef092a281
unknown <maryamjamal1703@gmail.com>
2025-07-21 10:55:50 +0300

    schedueledAt to schedueledFor in command model

--- domain/model/command.go
+++ domain/model/command.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Command struct {
-	ID          uint           `gorm:"primaryKey" json:"id"`
-	AreaID      *uint          `json:"area_id"`
-	LumiereID   *uint          `json:"lumiere_id"`
-	CommandData datatypes.JSON `gorm:"type:jsonb;not null" json:"command"`
-	ScheduledAt *time.Time     `gorm:"column:scheduled_for" json:"scheduled_for"`
-	ExecutedAt  *time.Time     `gorm:"column:executed_at" json:"executed_at"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	Area        *Area          `gorm:"foreignKey:AreaID" json:"-"`
-	Lumiere     *Lumiere       `gorm:"foreignKey:LumiereID" json:"-"`
+	ID           uint           `gorm:"primaryKey" json:"id"`
+	AreaID       *uint          `json:"area_id"`
+	LumiereID    *uint          `json:"lumiere_id"`
+	CommandData  datatypes.JSON `gorm:"type:jsonb;not null" json:"command"`
+	ScheduledFor *time.Time     `gorm:"column:scheduled_for" json:"scheduled_for"`
+	ExecutedAt   *time.Time     `gorm:"column:executed_at" json:"executed_at"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	Area         *Area          `gorm:"foreignKey:AreaID" json:"-"`
+	Lumiere      *Lumiere       `gorm:"foreignKey:LumiereID" json:"-"`
 }
